b170b2c5c590de4c0bff2eff52a4ab89218c5a71
Marco Cedaro <marco.cedaro@gmail.com>
2014-12-18 17:38:05 +0000

    fixed stupid logic error

--- modules/checkMovember.js
+++ modules/checkMovember.js
@@ -2,7 +2,7 @@ module.exports = function(beardData){
 	'use strict';
 	var shavedInNov = (beardData.lastShaved.month === 10);
 	var shavedInLastWeekOfOct = (beardData.lastShaved.month === 9 && beardData.lastShaved.day > 23);
-	var isItTooLate = beardData.daysSinceShaved < 40;
+	var isItTooLate = beardData.daysSinceShaved > 40;
 
 	return (shavedInNov || shavedInLastWeekOfOct) && !isItTooLate;
 };
